d393be0bc5e58667215e9aecac60cbf50f053e61
rlagkdms <ithaeun@naver.com>
2024-06-18 21:46:03 +0900

    style : home ui 수정

---
+++ js/config.js
@@ -0,0 +1,2 @@
+export default SERVER_HOST =
+  "http://ec2-3-35-19-10.ap-northeast-2.compute.amazonaws.com:8080";
--- js/home.js
+++ js/home.js
@@ -1,12 +1,12 @@
-let menuBtn = Array.from(document.querySelectorAll('.menu_bar input'));
-let menuStatics = Array.from(document.querySelectorAll('.static'));
+let menuBtn = Array.from(document.querySelectorAll(".menu_bar input"));
+let menuStatics = Array.from(document.querySelectorAll(".static"));
 console.log(menuBtn);
-console.log(menuStatics)
+console.log(menuStatics);
 
-//수정 필요 
-function showStatics (index){
-    for(static of menuStatics){
-        static.style.display = 'none'
-    }
-    menuStatics[index].style.display = 'block'
-} 
+//수정 필요
+function showStatics(index) {
+  for (static of menuStatics) {
+    static.style.display = "none";
+  }
+  menuStatics[index].style.display = "block";
+}
